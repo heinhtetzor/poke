4649af97eb148d24cfe7f60b44e7fcf79a5703a6
heinhtetzor <hhz18@icloud.com>
2021-12-08 21:19:42 +0800

    add tests

---
+++ test/poke-api.test.js
@@ -0,0 +1,26 @@
+const PokeWrapper = require("../lib/classes/poke-wrapper");
+
+describe("PokeApiWrapper", () => {
+	test ('return pokemon from poke api', async () => {
+		const pokeWrapper = new PokeWrapper({query: "pikachu"});
+		await pokeWrapper.getPokemon();	
+		expect(pokeWrapper.pokemonResponse.id).toEqual(25);	
+	})
+	
+	test ('return encounter details from poke api', async () => {
+		const pokeWrapper = new PokeWrapper({query: "pikachu"});
+		await pokeWrapper.getPokemon();	
+		await pokeWrapper.getEncounters();	
+		expect(pokeWrapper.encounterResponse[0].location_area.name).toEqual('trophy-garden-area');
+	})
+	
+	test ('same return value from id search and name search for same pokemon', async () => {
+		const pokeWrapper1 = new PokeWrapper({query: "pikachu"});
+		const pokeWrapper2 = new PokeWrapper({query: 25});
+	
+		await pokeWrapper1.getPokemon();
+		await pokeWrapper2.getPokemon();
+	
+		expect(pokeWrapper1.pokemonResponse.id).toEqual(pokeWrapper2.pokemonResponse.id);
+	})
+});
---
+++ test/utility.test.js
@@ -0,0 +1,52 @@
+const PokeWrapper = require("../lib/classes/poke-wrapper");
+const Utils = require("../lib/utils");
+
+describe ("Utility functions", () => {
+	test ("transform encounter details api response into simple location object", async () => {
+		const pokeWrapper = new PokeWrapper({ query: "snorlax" });
+		await pokeWrapper.init();
+
+		const locationObject = Utils.getEncountersByLocation(pokeWrapper.encounterResponse, "kanto");
+		const result =   {
+			'kanto-route-12-area': [ 'pokeflute' ],
+			'kanto-route-11-area': [ 'pokeflute' ],
+			'kanto-route-16-area': [ 'pokeflute' ]
+		}
+		expect(locationObject).toEqual(result);
+	});
+
+	test ("return empty object if kanto encounter location is not found", async () => {
+		const pokeWrapper = new PokeWrapper({ query: "dragonite" });
+		await pokeWrapper.init();
+		
+		const locationObject = Utils.getEncountersByLocation(pokeWrapper.encounterResponse, "kanto");
+		const result = {};
+		expect(locationObject).toEqual(result);
+	});
+	
+	test ("transform types api response into simple object", async () => {
+		const pokeWrapper = new PokeWrapper({ query: "456" });
+		await pokeWrapper.init();
+				
+		const typeObject = Utils.getTypes(pokeWrapper.pokemonResponse.types);
+		const result = ['water'];
+		expect(typeObject).toEqual(result);
+
+	});
+
+	test ("transform stats api response into simple object", async () => {
+		const pokeWrapper = new PokeWrapper({ query: "dragonite" });
+		await pokeWrapper.init();
+
+		const statObject = Utils.getStats(pokeWrapper.pokemonResponse.stats);		
+		const result = {
+			hp: 91,
+			attack: 134,
+			defense: 95,
+			'special-attack': 100,
+			'special-defense': 100,
+			speed: 80
+		  }
+		  expect(statObject).toEqual(result);
+	});
+})
